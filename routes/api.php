e80bb444268980ed70a06defa72fd918b94bec10
AbdullahAzza1 <abdullahazza141@gmail.com>
2024-02-07 17:54:00 +0100

    api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get("/prova", function () {
+    // $dati = [
+    //     "nome" => "Luca",
+    //     "cognome" => "Lambiase"
+    // ];
+    $dati = User::all();
+
+    return response()->json($dati);
 });
